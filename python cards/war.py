5df764de509bc1664723747c4deea3388ad04555
Shawn Osborne <shawnby222@gmail.com>
2024-01-31 15:18:33 -0600

    adds card game - war

--- python cards/war.py
+++ python cards/war.py
@@ -1,10 +1,23 @@
-import cardUtil
+import cardUtils
+
+def compare_cards(name1, card1, name2, card2, currentWar):
+    if card1.value > card2.value:
+        player_1.add_one(card2)
+        print(f'  {name1}\'s {card1} beats {name2}\'s {card2}') if currentWar else print(f'{name1}\'s {card1} beats {name2}\'s {card2}')
+    elif card2.value > card1.value:
+        player_2.add_one(card1)
+        print(f'  {name2}\'s {card2} beats {name1}\'s {card1}') if currentWar else print(f'{name2}\'s {card2} beats {name1}\'s {card1}') 
+    elif card1.value == card2.value:
+        print(f'  {name2}\'s {card2} equals {name1}\'s {card1}\n  **WAR**') if currentWar else print(f'{name2}\'s {card2} equals {name1}\'s {card1}\n  **WAR**')
+        return True
+    return False
+
 
 if __name__ == '__main__':
-    deck = cardUtil.Deck()
+    deck = cardUtils.Deck()
     deck.shuffle()
-    player_1 = cardUtil.Player('Player 1')
-    player_2 = cardUtil.Player('Player 2')
+    player_1 = cardUtils.Player('Player 1')
+    player_2 = cardUtils.Player('Player 2')
 
     player_1.add_one(deck.cards[0:26])
     player_2.add_one(deck.cards[26:])
@@ -13,8 +26,6 @@ if __name__ == '__main__':
     game_on = True
 
     while game_on:
-        war_on = False
-
         if len(player_1.hand) == 0:
             print(f'{player_1}\n{player_2}')
             print(f'{player_1.name} out of cards.\n{player_2.name} wins!!!')
@@ -29,16 +40,7 @@ if __name__ == '__main__':
         player_1_card = player_1.remove_one()
         player_2_card = player_2.remove_one()
 
-        if player_1_card.value > player_2_card.value:
-            player_1.add_one(player_2_card)
-            print(f'{player_1.name}\'s {player_1_card} beats {player_2.name}\'s {player_2_card}')
-        elif player_2_card.value > player_1_card.value:
-            player_2.add_one(player_1_card)
-            print(f'{player_2.name}\'s {player_2_card} beats {player_1.name}\'s {player_1_card}')
-        elif player_1_card.value == player_2_card.value:
-            war_on = True
-            print(f'{player_2.name}\'s {player_2_card} equals {player_1.name}\'s {player_1_card}\n  **WAR**')
-
+        war_on = compare_cards(player_1.name, player_1_card, player_2.name, player_2_card, False)
 
         while war_on:
             if (len(player_1.hand) < 4):
@@ -54,14 +56,5 @@ if __name__ == '__main__':
 
             player_1_cards = [player_1.remove_one() for _ in range(4)]
             player_2_cards = [player_2.remove_one() for _ in range(4)]
-            war_on = False
 
-            if player_1_cards[3].value > player_2_cards[3].value:
-                player_1.add_one(player_2_cards)
-                print(f'  {player_1.name}\'s {player_1_cards[3]} beats {player_2.name}\'s {player_2_cards[3]}')
-            elif player_2_cards[3].value > player_1_cards[3].value:
-                player_2.add_one(player_1_cards)
-                print(f'  {player_2.name}\'s {player_2_cards[3]} beats {player_1.name}\'s {player_1_cards[3]}')
-            elif player_1_cards[3].value == player_2_cards[3].value:
-                war_on = True
-                print(f'  {player_2.name}\'s {player_2_cards[3]} equals {player_1.name}\'s {player_1_cards[3]}\n  **WAR**')
+            war_on = compare_cards(player_1.name, player_1_cards[3], player_2.name, player_2_cards[3], True)
