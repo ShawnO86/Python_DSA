845a8c378c1cf9fcd5a78257790f3e40a0d1ef73
Shawn Osborne <shawnby222@gmail.com>
2024-03-06 13:48:10 -0600

    timing sorting and searching algorithms

--- chapter9.py
+++ chapter9.py
@@ -1,10 +1,10 @@
 from timeit import default_timer as timer
 
 
-#9.6.1 - selection sort O(n^2)
+#9.6.1 - Selection sort - O(n^2)
 def selection_sort(arr):
     #loops through entire list
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         #initialize smallest to index i
         index_smallest = i
         #loops through remaining list after index i
@@ -18,13 +18,14 @@ def selection_sort(arr):
         arr[i] = arr[index_smallest]
         #move arr[i] to where smallest num used to be
         arr[index_smallest] = temp
+        print(arr)
     return arr
 
 
-#9.7.1 - insertion sort - O(n^2) worst case, O(n) best case
+#9.7.1 - Insertion sort - O(n^2) worst case, O(n) best case
 def insertion_sort(arr):
-    #begin outer loop at idx 1, will switch value to idx 0 if temp is lower.
-    for i in range(1, len(arr)):
+    #begin outer loop at idx 1, to be able to switch value to idx 0 if lower.
+    for i in range(1, len(arr) - 1):
         #initialize unsorted position
         pos = i
         #set unsorted value to temp var
@@ -40,7 +41,7 @@ def insertion_sort(arr):
     return arr
 
 
-#book ch.4 - bubble sort O(n^2)
+#book ch.4 - Bubble sort - O(n^2)
 def bubble_sort(arr):
     #keeps track of last unsorted index
     unsorted_idx = len(arr) - 1
@@ -62,65 +63,137 @@ def bubble_sort(arr):
     return arr
 
 
-#9.8 - quck sort O(n*log(n))
-def quickSort(arr, left, right):
-    #base case: subarray has 1 or 0 elements
-    if right - left <= 0:
-        return
-    #obtain pivot index and partition array by last element in left part 
-    pos = partition(arr, left, right)
-    #partition and sort left of pivot
-    quickSort(arr, left, pos - 1)
-    #partition and sort right of pivot
-    quickSort(arr, pos + 1, right)
+#9.8 - Quick sort - O(n*log(n))
+def quick_sort(start, end, array):
+    #base case
+    if start < end:
+        #spot to divide array
+        pivot = partition(start, end, array)
+        #left array
+        quick_sort(start, pivot, array)
+        #right array
+        quick_sort(pivot + 1, end, array)
 
+#Book - Quick select - O(n)
+def quick_select(nth_lowest, start, end, array):
+    #base case
+    if end - start <= 0:
+        #if the partitioned array has only one element, nth_lowest has to be that value 
+        return array[start]
+    else:
+        #partition array from middle, sorting lower values to left and higher values to right
+        pivot_pos = partition(start, end, array)
+         
+        if nth_lowest < pivot_pos:
+            #call quick_select for left of pivot_pos until index is found (lower than pivot_pos) 
+            return quick_select(nth_lowest, start, pivot_pos - 1, array)
+        elif nth_lowest > pivot_pos:
+            #call quick_select for right of pivot_pos until index is found (higher than pivot_pos)
+            return quick_select(nth_lowest, pivot_pos + 1, end, array)
+        else:
+            #if pivotpos is in same spot as nth_lowest after the partition, index is found.
+            return array[pivot_pos]
 
-def partition(arr, left, right):
-    #right most index of array for pivot value
-    pivot_index = right
-    pivot = arr[pivot_index]
-    #start right pointer one before pivot index
-    right -= 1
+#partition helper for quicksort/quickselect - O(n) 
+def partition(start, end, array):
+    pivotPos = start + (end - start) // 2
+    pivot = array[pivotPos]
 
     while True:
-        #increment left pointer until the value is equal or greater than pivot value
-        while arr[left] < pivot:
-            left += 1
-        #decrement right pointer until the value is equal or less than pivot value
-        while arr[right] > pivot:
-            right -= 1
-        #check if left pointer has passed right pointer 
-        if left >= right:
-            '''When left becomes greater than or equal to right in the partition:
-            all elements to the left of the pivot are less than or equal to the pivot.
-            all elements to the right of the pivot are greater than or equal to the pivot.'''
+        while array[start] < pivot:
+            start += 1
+        while array[end] > pivot:
+            end -= 1
+        if start >= end:
             break
         else:
-            #swap left and right pointer values -- left is greater and right is less than pivot value due to previous while loops
-            arr[left], arr[right] = arr[right], arr[left]
+            array[start], array[end] = array[end], array[start]
+            start += 1
+            end -= 1
+    
+    return end
 
-    #left is greater than or equal to pivot value, so they get swapped, putting pivot value in the correct place at the end of left partition.
-    print('left:', arr[left], 'pivot:', arr[pivot_index])
-    print('b:', arr)
-    arr[pivot_index], arr[left] = arr[left], arr[pivot_index]
-    print('a:', arr)
+#9.9 Merge sort - O(n*log(n))
+def merge_sort(arr, left, right):
+    mid = 0
+    if left < right:
+        #midpoint of partition
+        mid = (left + right) // 2
+        #recursively sort left and right side
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        #merge left and right side in order
+        merge(arr, left, mid, right)
 
-    return left
+def merge(arr, left, mid, right):
+    #size of partition
+    part_size = right - left + 1
+    #temp array for merged nums
+    merged_nums = [0 for _ in range(part_size)]
+    #position to insert into merged nums
+    merge_pos = 0
+    #init left partition start position
+    left_pos = left
+    #init right partition start position
+    right_pos = mid + 1
+    #add smallest element to merged nums
+    while left_pos <= mid and right_pos <= right:
+        if arr[left_pos] < arr[right_pos]:
+            merged_nums[merge_pos] = arr[left_pos]
+            left_pos += 1
+        else:
+            merged_nums[merge_pos] = arr[right_pos]
+            right_pos += 1
+        merge_pos += 1
+    #if left pointer not to end of array
+    while left_pos <= mid:
+        merged_nums[merge_pos] = arr[left_pos]
+        left_pos += 1
+        merge_pos += 1
+    #if right pointer not to end of array
+    while right_pos <= right:
+        merged_nums[merge_pos] = arr[right_pos]
+        right_pos += 1   
+        merge_pos += 1
+    #copy merged nums back to original array
+    merge_pos = 0
+    while merge_pos < part_size:
+        arr[left + merge_pos] = merged_nums[merge_pos]
+        merge_pos += 1
+
+#9.14.1 LAB: Binary Search - O(log(n))
+def binary_search(array, target, left, right):
+    mid = left + (right - left) // 2
+    if target == array[mid]:
+        return mid
+    elif left == right:
+        return -1
+    else:
+        if array[mid] < target:
+            return binary_search(array, target, mid + 1, right)
+        else:
+            return binary_search(array, target, left, mid - 1)
+
 
 
 if __name__ == '__main__':
-    nums = [0, 5, 2, 1, 6, 3]
-    print('Unsorted array:', nums)
+    nums = [18, 12, 6, 2, 4, 3, 5, 1, 7, 0, 14]
+    #print('Unsorted array:', nums)
     start = timer()
     #selection_sort(nums)
     #insertion_sort(nums)
     #bubble_sort(nums)
-    quickSort(nums, 0, len(nums) - 1)
-    end = timer()
+    quick_sort(0, len(nums) - 1, nums)
+    #pos = quick_select(8, 0, len(nums) - 1, nums)
+    #merge_sort(nums, 0, len(nums) - 1)
+    #pos = binary_search(nums, 12, 0, len(nums) - 1)
+    print(nums)
+    #print(pos)
+    end = timer() 
     time = end - start
 
-    print('Sorted array:', nums)
-    print(f'Time taken: {time:.8f}ms')
+    #print('Sorted array:', nums)
+    print(f'Seconds taken: {time:.10f}')
 
 
 
